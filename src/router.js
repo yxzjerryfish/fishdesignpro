0de508cb981da539be9eb34fdeb0f605fdef30ff
fish <yxzjerryfish@hotmail.com>
2019-09-29 20:51:20 +0800

    初始化

--- src/router.js
+++ src/router.js
@@ -10,6 +10,24 @@ export default new Router({
   routes: [
     {
       path: "/",
+      component: { render: h => h("router-view") },
+      children: [
+        {
+          path: "/user/login",
+          namne: "login",
+          component: () =>
+            import(/* webpackChunkName: "User" */ "./views/User/Login.vue")
+        },
+        {
+          path: "/user/register",
+          namne: "register",
+          component: () =>
+            import(/* webpackChunkName: "User" */ "./views/User/Register.vue")
+        }
+      ]
+    },
+    {
+      path: "/home",
       name: "home",
       component: Home
     },
